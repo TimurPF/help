b48faa3891be95db126490311789901e71306e90
TimurPF <timurrozgonov2@gmail.com>
2024-12-06 18:18:29 +0200

    Delete docs directory

--- docs/feedback.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("callme-button").addEventListener("click", function(){
-    document.getElementById("feedback-menu").style.display = "block";
-});
-
-document.getElementById("close-feedback").addEventListener("click", function(){
-    document.getElementById("feedback-menu").style.display = "none";
-});
-
